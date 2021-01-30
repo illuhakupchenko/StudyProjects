5d854267263a2ed9e6a0c6d23aa595fd23fc981e
illuhakupchenko <illuhakupchenko@gmail.com>
2021-01-31 00:18:28 +0200

    refresh data

--- Python/OldProjects/Odessa's pojects/FirstProject/First/variables.py
+++
@@ -1,7 +0,0 @@
-num_1 = float(input("Enter first num: "))
-num_2 = float(input("Enter second num: "))
-res = float(num_1) + num_2
-Res = input("Enter something: ")
-Res *= 5
-print(Res)
-print("Result is", res)
